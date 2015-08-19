566f36ecfee8982c9d461681578f4333b8381e39
yangyang <yangyang@mofang.com>
2015-08-19 14:48:41 +0800

    wwwww

--- src/main/java/com/yy/test1/App.java
+++ src/main/java/com/yy/test1/App.java
@@ -6,6 +6,6 @@ package com.yy.test1;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World! 1111");
+		System.out.println("Hello World! 222");
 	}
 }
