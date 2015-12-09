ee2b7275483ef5d1cb40d6efd5b7757cea5b102d
yangyang <yangyang@mofang.com>
2015-12-10 00:03:42 +0800

    xxx

--- src/main/java/com/yy/test1/App.java
+++ src/main/java/com/yy/test1/App.java
@@ -1,15 +1,8 @@
 package com.yy.test1;
 
-/**
- * Hello world!
-<<<<<<< HEAD
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    } 
-    
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
 }
